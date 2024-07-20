ad22555669266b53ff1b290d97df563d8d08f0ab
rampo-kun <106396261+rampo-kun@users.noreply.github.com>
2024-07-20 18:26:05 +0530

    first commit

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <section>
+            <h1 className='head-text mb-10'>
+                Communities
+            </h1>
+        </section>
+    )
+}
+
+export default page
---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,57 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import UserCard from "@/components/cards/UserCard";
+
+async function page() {
+    const user = await currentUser();
+    if (!user) return null;
+
+    // fetch organization list created by user
+    const userInfo = await fetchUser(user.id);
+    if (!userInfo?.onBoarded) redirect("/onboarding");
+
+    //Fetch Users
+
+    const result = await fetchUsers(
+        {
+            userId: user.id,
+            searchString: '',
+            pageNumber: 1,
+            pageSize: 25
+        }
+    )
+
+    return (
+        <section>
+            <h1 className='head-text mb-10'>
+                Search
+            </h1>
+            {/* Search Bar */}
+
+            <div className="mt-14 flex flex-col gap-9">
+                {result.users.length === 0 ?
+                    (<p className="no-result">No Result</p>) : (
+                        <>
+                            {result.users.map((person) =>
+                            (
+                                <UserCard
+                                    key={person.id}
+                                    id={person.id}
+                                    name={person.name}
+                                    username={person.username}
+                                    imgUrl={person.image}
+                                    personType='User'
+                                />
+                            ))}
+                        </>
+                    )}
+            </div>
+
+        </section>
+    )
+}
+
+export default page
